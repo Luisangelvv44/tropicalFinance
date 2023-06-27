c313455a8a6015bd187c1c24aad0fce9cc4db772
Luisangelvv44 <luisangelvv44@gmail.com>
2023-06-26 23:26:20 -0400

    primer commit al repositorio

---
+++ src/components/footerComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <ion-tab-bar slot="bottom">
+            <ion-tab-button tab="tab1" href="/tabs/tab1">
+                <ion-icon aria-hidden="true" :icon="triangle" />
+                <ion-label>Tab 1</ion-label>
+            </ion-tab-button>
+
+            <ion-tab-button tab="tab2" href="/tabs/tab2">
+                <ion-icon aria-hidden="true" :icon="ellipse" />
+                <ion-label>Tab 2</ion-label>
+            </ion-tab-button>
+
+            <ion-tab-button tab="tab3" href="/tabs/tab3">
+                <ion-icon aria-hidden="true" :icon="square" />
+                <ion-label>Tab 3</ion-label>
+            </ion-tab-button>
+        </ion-tab-bar>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { IonTabBar, IonTabButton, IonLabel, IonIcon } from '@ionic/vue';
+    import { ellipse, square, triangle } from 'ionicons/icons';
+</script>
